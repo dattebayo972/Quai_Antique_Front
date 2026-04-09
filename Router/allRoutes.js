9455b99c2357b367c6650b2b6c26038f931c416d
Tristan <belizon.tristan@gmail.con>
2026-04-09 18:46:13 +0200

    Ajout de la page gallerie

--- Router/allRoutes.js
+++ Router/allRoutes.js
@@ -3,6 +3,11 @@ import Route from "./Route.js";
 //Définir ici vos routes
 export const allRoutes = [
     new Route("/", "Accueil", "../pages/home.html"),
+    new Route("/galerie", "Galerie", "../pages/galerie.html"),
+    new Route("/carte", "La carte", "../pages/carte.html"),
+    new Route("/reservations", "Les réservations", "../pages/reservations.html"),
+    new Route("/compte", "Mon compte", "../pages/compte.html"),
+    new Route("/connexion", "Connexion/Deconnexion", "../pages/connexion.html"),
 ];
 
 //Le titre s'affiche comme ceci : Route.titre - websitename
